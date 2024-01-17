3447f6096cbba5bf7076e1a028a92f804ac31549
anulax1225 <anulax1225@icloud.com>
2024-01-17 15:09:07 +0100

    Warpper around glm

--- bakara/src/bakara/math/math.h
+++ bakara/src/bakara/math/math.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#include "vec.h"
+#include "type.h"
---
+++ bakara/src/bakara/math/type.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "mathpch.h"
+
+namespace Bk::Math {
+    typedef glm::vec2 Vec2;
+    typedef glm::vec3 Vec3;
+    typedef glm::ve4 Vec4;
+    typedef glm::mat4 Mat4;
+    typedef glm::mat3 Mat3;    
+}
