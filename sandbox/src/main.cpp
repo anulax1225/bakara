cdb249fb0d75bd1660d4eb2f2bed73990fac35f9
anulax1225 <anulax1225@icloud.com>
2024-01-18 00:36:20 +0100

    Ajusted sandbox app to start a Bk Application

--- sandbox/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <bakara.h>
-
-int main() {
-    Bk::Log::Init();
-    BK_INFO("Bienvenue dans la sandbox {0}", "bakara");
-    Bk::Math::Vec2 vec(2.0f,2.0f);
-    std::cout << vec.x << " " << vec.y <<"\n";
-}
---
+++ sandbox/src/sandbox.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <bakara.h>
+
+class Sandbox : public Bk::Application 
+{
+    public:
+        Sandbox() 
+        {
+            
+        }
+
+        ~Sandbox() 
+        {
+
+        }
+};
+
+std::unique_ptr<Bk::Application> Bk::create_app() {
+    return std::unique_ptr<Bk::Application>(new Sandbox());
+}
