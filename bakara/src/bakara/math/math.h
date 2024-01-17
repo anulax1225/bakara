a237c02a390c65226d70772d9e86bae7d9bf0bde
anulax1225 <anulax1225@icloud.com>
2024-01-17 15:55:54 +0100

    Added submodule spdlog for logging messages

--- bakara/src/bakara/math/math.h
+++ bakara/src/bakara/math/math.h
@@ -1,3 +1,2 @@
 #pragma once
-
 #include "type.h"
--- bakara/src/bakara/math/type.h
+++ bakara/src/bakara/math/type.h
@@ -4,7 +4,7 @@
 namespace Bk::Math {
     typedef glm::vec2 Vec2;
     typedef glm::vec3 Vec3;
-    typedef glm::ve4 Vec4;
+    typedef glm::vec4 Vec4;
     typedef glm::mat4 Mat4;
     typedef glm::mat3 Mat3;    
 }
