0924a266562ec91d0729b9e3776cf233d3b8834d
anulax1225 <anulax1225@icloud.com>
2024-01-17 15:57:33 +0100

    Cleaned project

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <bakara/test.h>
-#include <bakara/math/vec.h>
+#include <bakara/math/math.h>
 
 int main() {
-    std::cout << Bk::Math::add(2,2) << "\n";
     Bk::Math::Vec2 vec(2.0f,2.0f);
     std::cout << vec.x << " " << vec.y <<"\n";
 }
