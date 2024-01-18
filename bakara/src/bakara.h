04c7dc9fea509a2ca7bd9b41f478fbf14ba50e39
anulax1225 <anulax1225@icloud.com>
2024-01-18 19:51:23 +0100

    Modified librerie headers and precompiled headers

--- bakara/src/bakara.h
+++ bakara/src/bakara.h
@@ -3,5 +3,10 @@
 #include <bakara/math/type.h>
 #include <bakara/core/log.h>
 #include <bakara/core/application.h>
+#include <bakara/events/event.h>
+#include <bakara/events/key_event.h>
+#include <bakara/events/window_event.h>
+#include <bakara/events/app_event.h>
+#include <bakara/core/key_codes.h>
 
 #include <bakara/core/entry.h>
--- bakara/src/bkpch.h
+++ bakara/src/bkpch.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <ostream>
 #include <utility>
 #include <memory>
 #include <functional>
 #include <string>
+#include <sstream>
+#include <format>
 #include <array>
 #include <vector>
+#include <type_traits>
+#include <exception>
+#include <cctype>
