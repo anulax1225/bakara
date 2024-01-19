4f7c680fe61618a9363cdb6d95eda5da0bda07c8
anulax1225 <anulax1225@icloud.com>
2024-01-19 11:22:26 +0100

    Test events and assertion

--- bakara/src/bakara.h
+++ bakara/src/bakara.h
@@ -3,10 +3,12 @@
 #include <bakara/math/type.h>
 #include <bakara/core/log.h>
 #include <bakara/core/application.h>
+#include <bakara/core/base.h>
+#include <bakara/core/io_codes.h>
 #include <bakara/events/event.h>
 #include <bakara/events/key_event.h>
+#include <bakara/events/mouse_event.h>
 #include <bakara/events/window_event.h>
 #include <bakara/events/app_event.h>
-#include <bakara/core/io_codes.h>
 
 #include <bakara/core/entry.h>
--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -10,7 +10,9 @@ class Sandbox : public Bk::Application
         {
             auto event = Bk::WindowResizeEvent(120, 120);
             auto key_event = Bk::KeyPressEvent(Bk::Key::A);
-            std::cout << event.to_string() << "\n" << key_event.to_string() << "\n";
+            auto mouse_event = Bk::MouseMoveEvent(2.3f, 1.2f);
+            std::cout << event.to_string() << "\n" << key_event.to_string() << "\n" << mouse_event.get_pos().x << "\n";
+            BK_CORE_IMP_ASSERT(false, "Big Error")
         }
 
         ~Sandbox() 
