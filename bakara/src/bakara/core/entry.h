d3740a64ab0b886ae9f72e14b563b59636d3de61
anulax1225 <anulax1225@icloud.com>
2024-01-19 11:18:31 +0100

    Modified tipo

--- bakara/src/bakara/core/entry.h
+++ bakara/src/bakara/core/entry.h
@@ -9,7 +9,7 @@ extern std::unique_ptr<Bk::Application> Bk::create_app();
 
 int main(int argc, char** argv) {
     Bk::Log::init();
-    BK_INFO("Bienvenue dans la sandbox {0} version {1}", "bakara", 1);
+    BK_CORE_INFO("Bienvenue dans la sandbox {0} version {1}", "bakara", 1);
     std::unique_ptr<Bk::Application> app = Bk::create_app();
     app->run();
     return 0;
