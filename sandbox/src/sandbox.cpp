2c00c4f90bc96309e5af06f7d82aa5708da8786a
anulax1225 <anulax1225@icloud.com>
2024-01-20 03:01:00 +0100

    Preparing for glfw window

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -8,11 +8,7 @@ class Sandbox : public Bk::Application
     public:
         Sandbox() 
         {
-            auto event = Bk::WindowResizeEvent(120, 120);
-            auto key_event = Bk::KeyPressEvent(Bk::Key::A);
-            auto mouse_event = Bk::MouseMoveEvent(2.3f, 1.2f);
-            std::cout << event.to_string() << "\n" << key_event.to_string() << "\n" << mouse_event.get_pos().x << "\n";
-            BK_VA_MSG_ASSERT(5 == 6, "Les lois de la physique ne sont pas {0}", "boulversé.")
+          
         }
 
         ~Sandbox() 
