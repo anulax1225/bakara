f82851d6d31cf98d900dc9da6848b96e9780c012
ambigipathyv <vinayak.ambigapathy@rpn.ch>
2024-01-22 14:46:52 +0100

    Tested glm lib, glfw and opengl on windows

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -9,7 +9,8 @@ class Sandbox : public Bk::Application
     public:
         Sandbox() 
         {
-            
+            Bk::Vec2 vec(2.0f, 2.0f);
+            BK_INFO("My Vec 2 ({0}, {1})", vec.x, vec.y); 
         }
 
         ~Sandbox() 
