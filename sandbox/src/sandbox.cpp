977fd67ddf308b80cda5a4b7660418f2f425d608
anulax1225 <anulax1225@icloud.com>
2024-01-23 23:14:19 +0100

    Added test layer to the sandbox

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -1,15 +1,48 @@
-#include <iostream>
 #include <memory>
 #include <utility>
 #include <string>
 #include <bakara.h>
 
+#include <bakara/events/events.h>
+
+//USE THE FUCKING NAMESPACE OR BREAK
+
+class TestLayer : public Bk::Layer
+{
+    public:
+        TestLayer()
+            : Layer("Test") {}
+
+        void on_attach() override
+        {
+            BK_INFO("Layer {} attached", name);
+        }
+
+        void on_detach() override
+        {
+            BK_INFO("Layer {} detached", name);
+        }
+
+        void on_event(Bk::Event& e) override
+        {
+            BK_INFO("Layer {} event", name);
+        }
+
+        void on_update() override
+        {
+            BK_INFO("Layer {} updated", name);
+        }
+};
 
 class Sandbox : public Bk::Application 
 {
     public:
         Sandbox() 
-        {
+        { 
+            push_layer(new TestLayer());
+            push_layer(new TestLayer());
+            std::unique_ptr<Bk::Layer> layer = pop_overlay();
+            BK_INFO("Layer retirer et récupérer {0}", layer->to_string());
 
         }
 
@@ -18,6 +51,8 @@ class Sandbox : public Bk::Application
 
         }
 };
+
+
 
 std::unique_ptr<Bk::Application> Bk::create_app() {
     return std::unique_ptr<Bk::Application>(new Sandbox());
