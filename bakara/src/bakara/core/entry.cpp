807995281daab708bf726b1b6625f33fd0b8fe88
anulax1225 <anulax1225@icloud.com>
2024-01-29 16:06:37 +0100

    Added documentation to the programs entry

--- bakara/src/bakara/core/entry.cpp
+++ bakara/src/bakara/core/entry.cpp
@@ -2,9 +2,14 @@
 #include "log.h"
 #include "application.h"
 
-
+/*! \fn std::unique_ptr<Bk::Application> Bk::create_app()
+External function implemented client side.
+*/
 extern std::unique_ptr<Bk::Application> Bk::create_app();
 
+/*! \fn int main(int argc, char** argv)
+Entry of the program.
+*/
 int main(int argc, char** argv) {
     Bk::Log::init();
     BK_CORE_INFO("Bienvenue dans la sandbox {0} version {1}", "bakara", 1);
