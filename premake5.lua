9d782283f95297ff51ebe98bc00000bd51700839
anulax1225 <anulax1225@icloud.com>
2024-06-05 15:20:51 +0200

    Added bakasable config

--- premake5.lua
+++ premake5.lua
@@ -1,39 +1,95 @@
-workspace "Bakara"
-    architecture "x64"
-    configurations { "Debug", "Release" }
-    startproject "sandbox"
+project "bakara"
+    language "C++"
+    cppdialect "C++17"
 
-    flags
-	{
-		"MultiProcessorCompile"
-	}
-
-    linkgroups "On"
-
-outputdir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}"
-
-IncludeDirs = {}
-IncludeDirs["bakara"] = "%{wks.location}/bakara/src"
-IncludeDirs["vendor"] = "%{wks.location}/bakara/vendor"
-IncludeDirs["spdlog"] = "%{wks.location}/bakara/vendor/spdloglib/include"
-IncludeDirs["glm"] = "%{wks.location}/bakara/vendor/glm"
-IncludeDirs["glfw"] = "%{wks.location}/bakara/vendor/glfw/include"
-IncludeDirs["glad"] = "%{wks.location}/bakara/vendor/glad/include"
-IncludeDirs["imgui"] = "%{wks.location}/bakara/vendor/imgui"
+    targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 
-group "Dependencies"
-    include "bakara/vendor"
-group ""
+    files 
+    {
+        "vendor/glm/glm/**.hpp",
+        "vendor/glm/glm/**.inl",
+        "vendor/imgui/misc/cpp/imgui_stdlib.cpp",
+        "vendor/imgui/backends/imgui_impl_opengl3.cpp",
+        "vendor/imgui/backends/imgui_impl_glfw.cpp",
+        "src/**.h",
+        "src/**.cpp",
+    }
 
-group "Core"
-	include "bakara"
-group ""
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "GLFW_INCLUDE_NONE",
+        "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+    }
 
-group "App"
-	include "sandbox"
-group ""
+    includedirs 
+    {
+        "%{IncludeDirs.bakara}",
+        "%{IncludeDirs.glm}",
+        "%{IncludeDirs.spdlog}",
+        "%{IncludeDirs.glad}",
+        "%{IncludeDirs.glfw}",
+        "%{IncludeDirs.imgui}"
+
+    }
+
+    links 
+    {
+        "GLFW",
+        "GLAD",
+        "ImGui"
+    }
+
+    filter "configurations:Debug"
+        defines 
+        { 
+            "BK_DEBUG",
+            "DEBUG"
+        }
+        runtime "Debug"
+        symbols "on"
 
 
+    filter "configurations:Release"
+        defines 
+        { 
+            "BK_RELEASE",
+            "NDEBUG"
+        }
+        runtime "Release"
+        optimize "on"
 
+    filter "system:windows"
+        kind "StaticLib"
+        staticruntime "on"
+        defines 
+        { 
+            "BK_PLATFORM_WINDOWS" 
+        }
+
+        links
+        {
+            "opengl32.lib"
+        }
+
+    filter "system:linux"
+        kind "SharedLib"
+        staticruntime "off"
+
+        files
+        {
+            "vendor/glm/glm/**.cpp"
+        }
+
+        defines 
+        { 
+            "BK_PLATFORM_LINUX" 
+        }
+
+        links 
+        {
+            "GL"
+        }
 
